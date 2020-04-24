4cfe275be95da4aecbe782136b8cbe03adebd5c3
Urland-LMB <64139850+Urland-LMB@users.noreply.github.com>
2020-04-24 08:47:30 +0200

    Flere kort :)

--- layers/layers.js
+++ layers/layers.js
@@ -39,29 +39,68 @@ var lyr_20032006A_2 = new ol.layer.Image({
                                 imageExtent: [892435.331682, 7254394.028628, 1734560.279975, 7917640.423656]
                             })
                         });
-var format_DanmarkOutline_3 = new ol.format.GeoJSON();
-var features_DanmarkOutline_3 = format_DanmarkOutline_3.readFeatures(json_DanmarkOutline_3, 
+var lyr_20072010A_3 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "2007-2010 A",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/20072010A_3.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [892435.331682, 7254394.028628, 1734560.279975, 7917640.423656]
+                            })
+                        });
+var lyr_20112014A_4 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "2011-2014 A",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/20112014A_4.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [892435.331682, 7254394.028628, 1734560.279975, 7917640.423656]
+                            })
+                        });
+var lyr_20152018A_5 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "2015-2018 A",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/20152018A_5.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [892435.331682, 7254394.028628, 1734560.279975, 7917640.423656]
+                            })
+                        });
+var format_DanmarkOutline_6 = new ol.format.GeoJSON();
+var features_DanmarkOutline_6 = format_DanmarkOutline_6.readFeatures(json_DanmarkOutline_6, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_DanmarkOutline_3 = new ol.source.Vector({
+var jsonSource_DanmarkOutline_6 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_DanmarkOutline_3.addFeatures(features_DanmarkOutline_3);
-var lyr_DanmarkOutline_3 = new ol.layer.Vector({
+jsonSource_DanmarkOutline_6.addFeatures(features_DanmarkOutline_6);
+var lyr_DanmarkOutline_6 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_DanmarkOutline_3, 
-                style: style_DanmarkOutline_3,
+                source:jsonSource_DanmarkOutline_6, 
+                style: style_DanmarkOutline_6,
                 interactive: false,
-                title: '<img src="styles/legend/DanmarkOutline_3.png" /> Danmark Outline'
+                title: '<img src="styles/legend/DanmarkOutline_6.png" /> Danmark Outline'
             });
 var group_Boligpriskortcropped = new ol.layer.Group({
-                                layers: [lyr_19951998A_0,lyr_19992002A_1,lyr_20032006A_2,],
+                                layers: [lyr_19951998A_0,lyr_19992002A_1,lyr_20032006A_2,lyr_20072010A_3,lyr_20112014A_4,lyr_20152018A_5,],
                                 title: "Boligpriskort cropped"});
 
-lyr_19951998A_0.setVisible(true);lyr_19992002A_1.setVisible(true);lyr_20032006A_2.setVisible(true);lyr_DanmarkOutline_3.setVisible(true);
-var layersList = [group_Boligpriskortcropped,lyr_DanmarkOutline_3];
-lyr_DanmarkOutline_3.set('fieldAliases', {'FEAT_KODE': 'FEAT_KODE', 'FEAT_TYPE': 'FEAT_TYPE', 'FEAT_STTXT': 'FEAT_STTXT', 'GEOM_STTXT': 'GEOM_STTXT', 'DQ_INDEX': 'DQ_INDEX', 'TIMEOF_CRE': 'TIMEOF_CRE', 'TIMEOF_PUB': 'TIMEOF_PUB', 'TIMEOF_REV': 'TIMEOF_REV', 'TIMEOF_EXP': 'TIMEOF_EXP', });
-lyr_DanmarkOutline_3.set('fieldImages', {'FEAT_KODE': '', 'FEAT_TYPE': '', 'FEAT_STTXT': '', 'GEOM_STTXT': '', 'DQ_INDEX': '', 'TIMEOF_CRE': '', 'TIMEOF_PUB': '', 'TIMEOF_REV': '', 'TIMEOF_EXP': '', });
-lyr_DanmarkOutline_3.set('fieldLabels', {'FEAT_KODE': 'no label', 'FEAT_TYPE': 'no label', 'FEAT_STTXT': 'no label', 'GEOM_STTXT': 'no label', 'DQ_INDEX': 'no label', 'TIMEOF_CRE': 'no label', 'TIMEOF_PUB': 'no label', 'TIMEOF_REV': 'no label', 'TIMEOF_EXP': 'no label', });
-lyr_DanmarkOutline_3.on('precompose', function(evt) {
+lyr_19951998A_0.setVisible(true);lyr_19992002A_1.setVisible(true);lyr_20032006A_2.setVisible(true);lyr_20072010A_3.setVisible(true);lyr_20112014A_4.setVisible(true);lyr_20152018A_5.setVisible(true);lyr_DanmarkOutline_6.setVisible(true);
+var layersList = [group_Boligpriskortcropped,lyr_DanmarkOutline_6];
+lyr_DanmarkOutline_6.set('fieldAliases', {'FEAT_KODE': 'FEAT_KODE', 'FEAT_TYPE': 'FEAT_TYPE', 'FEAT_STTXT': 'FEAT_STTXT', 'GEOM_STTXT': 'GEOM_STTXT', 'DQ_INDEX': 'DQ_INDEX', 'TIMEOF_CRE': 'TIMEOF_CRE', 'TIMEOF_PUB': 'TIMEOF_PUB', 'TIMEOF_REV': 'TIMEOF_REV', 'TIMEOF_EXP': 'TIMEOF_EXP', });
+lyr_DanmarkOutline_6.set('fieldImages', {'FEAT_KODE': '', 'FEAT_TYPE': '', 'FEAT_STTXT': '', 'GEOM_STTXT': '', 'DQ_INDEX': '', 'TIMEOF_CRE': '', 'TIMEOF_PUB': '', 'TIMEOF_REV': '', 'TIMEOF_EXP': '', });
+lyr_DanmarkOutline_6.set('fieldLabels', {'FEAT_KODE': 'no label', 'FEAT_TYPE': 'no label', 'FEAT_STTXT': 'no label', 'GEOM_STTXT': 'no label', 'DQ_INDEX': 'no label', 'TIMEOF_CRE': 'no label', 'TIMEOF_PUB': 'no label', 'TIMEOF_REV': 'no label', 'TIMEOF_EXP': 'no label', });
+lyr_DanmarkOutline_6.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
